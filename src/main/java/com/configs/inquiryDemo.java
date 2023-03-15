4b14f2192e55d7ad17d99f53e2a14a8590faf2a9
Nguyễn Thị Hà Xuân <68209819+Deserve-040900@users.noreply.github.com>
2023-03-15 09:46:07 +0700

    update to uppercase

--- src/main/java/com/configs/inquiryDemo.java
+++ src/main/java/com/configs/inquiryDemo.java
@@ -10,10 +10,12 @@ public class inquiryDemo {
 	public static void main(String[] args) {
 		SessionFactory f = HibernateUtils.getFactory();
 		
-		try (Session ss = f.openSession()){
-			Category c = new Category(); // transient
-			c.setName("Tablet");//			
-			ss.save(c); // insert
+		try (Session ss = f.openSession()){			
+			Category c = ss.get(Category.class, 7); // persistent
+			c.setName("GAMING");	
+			ss.getTransaction().begin(); // open transaction
+			ss.save(c); // update
+			ss.getTransaction().commit(); // push data on cache to DB
 		}
 		
 		//ss.close();
