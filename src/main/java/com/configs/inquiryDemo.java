c5a44374083bc63640e736c310e4e9ecaff14a1b
Nguyễn Thị Hà Xuân <68209819+Deserve-040900@users.noreply.github.com>
2023-03-15 10:38:07 +0700

    query List all data

--- src/main/java/com/configs/inquiryDemo.java
+++ src/main/java/com/configs/inquiryDemo.java
@@ -1,20 +1,24 @@
 package com.configs;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import com.pojos.Adminuser;
 import com.pojos.Category;
 
+import jakarta.persistence.Query;
+
 public class inquiryDemo {
 	public static void main(String[] args) {
 		SessionFactory f = HibernateUtils.getFactory();
 		
-		try (Session ss = f.openSession()){			
-			Category c = ss.get(Category.class, 7);
-			ss.getTransaction().begin();
-			ss.delete(c);
-			ss.getTransaction().commit();
+		try (Session ss = f.openSession()){
+			Query q = ss.createQuery("From Category");
+			List<Category> rs = q.getResultList();
+			
+			rs.forEach(c -> System.out.printf("%d - %s \n", c.getId(), c.getName()));
 		}
 		
 		//ss.close();
