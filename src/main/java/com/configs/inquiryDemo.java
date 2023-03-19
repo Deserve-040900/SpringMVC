ba5bf013d7487e5ec30951f150c37313823812b4
Nguyễn Thị Hà Xuân <68209819+Deserve-040900@users.noreply.github.com>
2023-03-19 19:55:15 +0700

    Hibernate Query Language (HQL)

--- src/main/java/com/configs/inquiryDemo.java
+++ src/main/java/com/configs/inquiryDemo.java
@@ -1,6 +1,7 @@
 package com.configs;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -9,6 +10,7 @@ import org.hibernate.SessionFactory;
 import com.pojos.Adminuser;
 import com.pojos.Category;
 import com.pojos.Product;
+import com.pojos.Tag;
 
 import jakarta.persistence.Query;
 
@@ -17,14 +19,23 @@ public class inquiryDemo {
 		SessionFactory f = HibernateUtils.getFactory();
 		
 		try (Session ss = f.openSession()){
-			Category c = ss.get(Category.class, 5); // persistent object
+			Category c = ss.get(Category.class, 2);
+			Tag t1 = ss.get(Tag.class, 1);
+			Tag t2 = ss.get(Tag.class, 2);
 			
 			Product p = new Product();
-			p.setName("iPad mini 2023");
-			p.setPrice(new BigDecimal(12000000));
+			p.setName("MacBook note pro 2023");
+			p.setPrice(new BigDecimal(32000000));
 			p.setCategoryId(c);
 			
+			List<Tag> t = new ArrayList<>();
+			t.add(t1);
+			t.add(t2);
+			p.setTags(t);
+			// open transaction to map all data to database
+			ss.getTransaction().begin();
 			ss.save(p);
+			ss.getTransaction().commit();
 		}
 		
 		//ss.close();
