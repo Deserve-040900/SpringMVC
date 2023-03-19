4a0aa5a6e65f4f510aa5b095dc855b3e8ee343b6
Nguyễn Thị Hà Xuân <68209819+Deserve-040900@users.noreply.github.com>
2023-03-19 21:09:32 +0700

    criteria query list product

--- src/main/java/com/configs/inquiryDemo.java
+++ src/main/java/com/configs/inquiryDemo.java
@@ -7,35 +7,30 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import com.pojos.Adminuser;
 import com.pojos.Category;
 import com.pojos.Product;
 import com.pojos.Tag;
 
 import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 public class inquiryDemo {
 	public static void main(String[] args) {
 		SessionFactory f = HibernateUtils.getFactory();
 		
 		try (Session ss = f.openSession()){
-			Category c = ss.get(Category.class, 2);
-			Tag t1 = ss.get(Tag.class, 1);
-			Tag t2 = ss.get(Tag.class, 2);
+			CriteriaBuilder cb = ss.getCriteriaBuilder();
+			CriteriaQuery<Product> query = cb.createQuery(Product.class);
+			Root rt = query.from(Product.class);
+			query.select(rt);
 			
-			Product p = new Product();
-			p.setName("MacBook note pro 2023");
-			p.setPrice(new BigDecimal(32000000));
-			p.setCategoryId(c);
-			
-			List<Tag> t = new ArrayList<>();
-			t.add(t1);
-			t.add(t2);
-			p.setTags(t);
-			// open transaction to map all data to database
-			ss.getTransaction().begin();
-			ss.save(p);
-			ss.getTransaction().commit();
+			Query q = ss.createQuery(query);
+			List<Product> prod = q.getResultList();
+			prod.forEach(p -> {
+				System.out.printf("%d - %s - %.1f VND - %s \n", p.getId(), p.getName(), p.getPrice(), p.getCategoryId().getName());
+			});
 		}
 		
 		//ss.close();
