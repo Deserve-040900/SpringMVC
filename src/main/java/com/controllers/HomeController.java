c3aa1619d327713d72522624f91d96e02c8e0ff7
Nguyễn Thị Hà Xuân <68209819+Deserve-040900@users.noreply.github.com>
2023-03-20 11:25:36 +0700

    http POST ModelAttribute

--- src/main/java/com/controllers/HomeController.java
+++ src/main/java/com/controllers/HomeController.java
@@ -4,10 +4,15 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.pojos.user;
 
 @Controller
 public class HomeController {
@@ -30,4 +35,22 @@ public class HomeController {
 		model.addAttribute("name", fn + ' ' + ln);
 		return "index";
 	}
+	
+	@GetMapping("/login")
+	public String loginView(Model model) {
+		model.addAttribute("user", new user());
+		return "login";
+	}
+	@PostMapping("/login")
+	public String loginHandler(Model model, @ModelAttribute(value = "user") user users) {
+		if (users.getUsername().equals("admin") && users.getPassword().equals("12345")) {
+			model.addAttribute("msg", "SUCCESSFUL");
+		} else { model.addAttribute("msg", "FAILED!!"); }
+		return "login";
+	}
+	// http GET --> get resources (public) --> URL?...
+	// http POST --> add resources (private) --> http BODY
+	// http PUT --> update (the whole resources)
+	// http PATCH --> update (a part of resources)
+	// http DELETE --> delete resources
 }
