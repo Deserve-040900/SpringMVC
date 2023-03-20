8a4ebaeefccf16fea3737349aa61aade8c125170
Nguyễn Thị Hà Xuân <68209819+Deserve-040900@users.noreply.github.com>
2023-03-20 10:04:57 +0700

    path parameter

--- src/main/java/com/controllers/HomeController.java
+++ src/main/java/com/controllers/HomeController.java
@@ -1,12 +1,22 @@
 package com.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/home")
-	public String index() {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("name", "Project Maven");
+		return "index";
+	}
+	
+	@RequestMapping(value = "/demo/{name}")
+	public String test(Model model, @PathVariable(name = "name") String name) {
+		model.addAttribute("name", name);
 		return "index";
 	}
 }
